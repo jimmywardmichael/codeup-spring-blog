871665b518315e0a48cc86ba166efde28b00c68c
James Ward <jimmywardmichael@gmail.com>
2023-02-16 15:38:28 -0600

    HomeController exercise done

--- src/main/java/com/codeup/codeupspringblog/HomeController.java
+++ src/main/java/com/codeup/codeupspringblog/HomeController.java
@@ -1,6 +1,10 @@
 package com.codeup.codeupspringblog;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+@Controller
 public class HomeController {
-
+    @GetMapping(path = "landing")
+    @ResponseBody
+    public String landingPage(){
+        return "<h1>This is the landing page!</h1>";}
 }
